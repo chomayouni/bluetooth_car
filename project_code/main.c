b577d93e504f6d34b436befd9a5f9938bd9cf066
chomayouni <69778851+chomayouni@users.noreply.github.com>
2024-11-14 19:31:58 -0500

    Remove unused Bluetooth and motor control files

--- project_code/main.c
+++ project_code/main.c
@@ -1,48 +1,161 @@
-// File: main.c
-#include "main.h"
-#include "C:\Keil_v5\Lab 2\inc/tm4c123gh6pm.h"
-// #include "motor_control.h"
-// #include "bluetooth_comm.h"
-// #include "state_machine.h"
 
-void Init_IO(void) {
-    // Activate clocks for Port B and Port E
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGC2_GPIOB | SYSCTL_RCGC2_GPIOE;
+#include "main.h"
+
+
+void GPIO_Init(void);
+void Init_PWM(void);
+void HC05_init(void);
+
+void Init_BT(void);
+char Bluetooth_Read(void);
+void Bluetooth_Write(unsigned char data);
+void Bluetooth_Write_String(char *str);
+
+void LED_Control(char direction);
+
+void moveForward(long direction, unsigned long speed, unsigned int duration);
+void moveBackward(long direction, unsigned long speed, unsigned int duration);
+void tankTurnLeft(long angle, unsigned int speed);
+void tankTurnRight(long angle, unsigned int speed);
+void stopMotors(void);
+
+void delay(uint32_t count);
+void delayms(uint32_t count);
+
+int main(void) {
+		// default speed and direction
+		long cur_direction = 0;
+		unsigned long cur_speed = 8000;
+		unsigned int cur_duration = 1000;
+
+	
+    // Initialize all subsystems
+	  HC05_init();
+    GPIO_Init();
+    Init_PWM();
+	
+    // Send welcome message and controls
+    Bluetooth_Write_String("Connection initialized. Car is under your Control!\n");
+    Bluetooth_Write_String("Controls:\n");
+    Bluetooth_Write_String("W - Forward\n");
+    Bluetooth_Write_String("S - Backward\n");
+    Bluetooth_Write_String("A - Left\n");
+    Bluetooth_Write_String("D - Right\n");
     
-    // Wait for clock to stabilize
-    while((SYSCTL_PRGPIO_R & (SYSCTL_RCGC2_GPIOB | SYSCTL_RCGC2_GPIOE)) == 0){}
+    while(1)
+    {
+        char c = Bluetooth_Read();
+        
+        switch(c) {
+            case 'W':
+            case 'w':
+                LED_Control('W');  // Set LED to indicate forward (Blue)
+                Bluetooth_Write_String("Moving Forward\n");
+								moveForward(cur_direction,cur_speed,cur_duration);
+                break;
+                
+            case 'S':
+            case 's':
+                LED_Control('S');  // Set LED to indicate backward (Red)
+                Bluetooth_Write_String("Moving Backward\n");
+								moveBackward(cur_direction,cur_speed,cur_duration);
+                break;
+                
+            case 'A':
+            case 'a':
+                LED_Control('A');  // Set LED to indicate left (Green)
+                Bluetooth_Write_String("Moving Left\n");
+								tankTurnLeft(90,cur_speed);
+                break;
+                
+            case 'D':
+            case 'd':
+                LED_Control('D');  // Set LED to indicate right (All LEDs)
+                Bluetooth_Write_String("Moving Right\n");
+								tankTurnRight(90,cur_speed);
+                break;
+        }
+    }
+}
+void GPIO_Init(void) {
+	  SYSCTL->RCGCGPIO |= 0x20;   // Enable clock to GPIOF
+    delay(1);
     
-    // Configure Port B pins for motor control
-    GPIO_PORTB_DIR_R |= (MASK(IN_13R)|MASK(IN_24R)|MASK(EN_R)|
-                        MASK(IN_13L)|MASK(IN_24L)|MASK(EN_L));  // Set as outputs
-    GPIO_PORTB_DEN_R |= (MASK(IN_13R)|MASK(IN_24R)|MASK(EN_R)|
-                        MASK(IN_13L)|MASK(IN_24L)|MASK(EN_L));  // Enable digital I/O
+    // Unlock PF0 (SW2)
+    GPIOF->LOCK = 0x4C4F434B;
+    GPIOF->CR = 0x1F;
     
-    // Configure Port E pins for UART
-    GPIO_PORTE_DEN_R |= (MASK(TX)|MASK(RX));  // Enable digital I/O for UART pins
+    GPIOF->DIR |= 0x0E;         // Set PF1, PF2, PF3 as outputs (LEDs)
+    GPIOF->DEN |= 0x0E;         // Enable digital functions for PF1, PF2, PF3
+    GPIOF->DATA &= ~0x0E;       // Turn off all LEDs initially
+ 
 }
 
 void Init_PWM(void) {
+		// designed around 16 MHz default clock
+	
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGC2_GPIOB; // Active clock for port B
+		// wait for clock to stabilize
+		while((SYSCTL_PRGPIO_R & SYSCTL_RCGC2_GPIOB) == 0){};
+			
+		// Configure Port B pins for motor control
+    GPIO_PORTB_DIR_R |= (MASK(IN_13R)|MASK(IN_24R)|MASK(IN_13L)|
+                        MASK(IN_24L));  // Set as outputs
+    GPIO_PORTB_DEN_R |= (MASK(IN_13R)|MASK(IN_24R)|MASK(IN_13L)|
+                        MASK(IN_24L));  // Enable digital I/O
+			
     // Enable PWM0 module
     SYSCTL_RCGC0_R |= SYSCTL_RCGC0_PWM0;
-    
+ 
     // Wait for clock to stabilize
-    while((SYSCTL_RCGC0_R & SYSCTL_RCGC0_PWM0) == 0){}
+    while((SYSCTL_RCGC0_R & SYSCTL_RCGC0_PWM0) == 0){};
     
+		// change PWM CLK to 1 MHz, 16MHz/16 = 1 MHz
+		SYSCTL_RCC_R|=	SYSCTL_RCC_USEPWMDIV | SYSCTL_RCC_PWMDIV_32;
+		
+    // Wait for clock to stabilize
+    while((SYSCTL_RCC_R & (SYSCTL_RCC_USEPWMDIV | SYSCTL_RCC_PWMDIV_32)) == 0){};			
+			
     // Configure PWM pins
     GPIO_PORTB_AFSEL_R |= (MASK(EN_R)|MASK(EN_L));  // Enable alternate function
-    GPIO_PORTB_PCTL_R &= ~(0xFF << (EN_R*4));       // Clear PCTL bits
+    GPIO_PORTB_PCTL_R &= ~(0xF << (EN_R*4));       // Clear PCTL bits
     GPIO_PORTB_PCTL_R |= (4 << (EN_R*4));           // Set PCTL for PWM
-    GPIO_PORTB_PCTL_R &= ~(0xFF << (EN_L*4));       // Clear PCTL bits
+    GPIO_PORTB_PCTL_R &= ~(0xF << (EN_L*4));       // Clear PCTL bits
     GPIO_PORTB_PCTL_R |= (4 << (EN_L*4));           // Set PCTL for PWM
-    
+			
+    GPIO_PORTB_AMSEL_R &= ~(MASK(EN_R)|MASK(EN_L)) ; // disable analog function for pins
+		GPIO_PORTB_DEN_R |= (MASK(EN_R)|MASK(EN_L)); // Enable digital output
+			
     // Configure PWM generators
     PWM0_0_CTL_R = 0;                    // Disable PWM while configuring
-    PWM0_0_GENA_R = 0x0000008C;          // Set PWM output when counter=LOAD, clear when matches CMP
-    PWM0_0_LOAD_R = 16000;               // Set load value for 1kHz (16MHz/16000)
-    PWM0_0_CMPA_R = 8000;                // Set duty cycle to 50% initially
+    PWM0_0_GENA_R = PWM_0_GENA_ACTLOAD_ONE|PWM_0_GENA_ACTCMPAD_ZERO;          // Set PWM output when counter=LOAD, clear when matches CMPA
+		PWM0_0_GENB_R = PWM_0_GENB_ACTLOAD_ONE|PWM_0_GENB_ACTCMPBD_ZERO;          // Set PWM output when counter=LOAD, clear when matches CMPB
+    PWM0_0_LOAD_R = 16667;               // Set load value for 60Hz (1MHz/16667)
+    PWM0_0_CMPA_R = CMP_Max;           // Set R duty cycle to ~0% initially
+		PWM0_0_CMPB_R = CMP_Max;           // Set L duty cycle to ~0% initially
     PWM0_0_CTL_R = 1;                    // Enable PWM0
-    PWM0_ENABLE_R |= PWM_ENABLE_PWM0EN;  // Enable PWM0 outputs
+    PWM0_ENABLE_R |= PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN;  // Enable PWM0 outputs
+		PWM0_ENABLE_R &= ~(PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Disable PWM0 outputs
+}
+
+void HC05_init(void)
+{
+    SYSCTL->RCGCUART |= 0x20;  /* enable clock to UART5 */
+    SYSCTL->RCGCGPIO |= 0x10;  /* enable clock to PORTE for PE4/Rx and RE5/Tx */
+    delay(1);
+    /* UART0 initialization */
+    UART5->CTL = 0;         /* UART5 module disbable */
+    UART5->IBRD = 104;      /* for 9600 baud rate, integer = 104 */
+    UART5->FBRD = 11;       /* for 9600 baud rate, fractional = 11*/
+    UART5->CC = 0;          /*select system clock*/
+    UART5->LCRH = 0x60;     /* data lenght 8-bit, not parity bit, no FIFO */
+    UART5->CTL = 0x301;     /* Enable UART5 module, Rx and Tx */
+
+    /* UART5 TX5 and RX5 use PE4 and PE5. Configure them digital and enable alternate function */
+    GPIOE->DEN = 0x30;      /* set PE4 and PE5 as digital */
+    GPIOE->AFSEL = 0x30;    /* Use PE4,PE5 alternate function */
+    GPIOE->AMSEL = 0;    /* Turn off analg function*/
+    GPIOE->PCTL = 0x00110000;     /* configure PE4 and PE5 for UART */
 }
 
 void Init_BT(void) {
@@ -51,9 +164,9 @@ void Init_BT(void) {
     
     // Configure UART pins
     GPIO_PORTE_AFSEL_R |= (MASK(TX)|MASK(RX));  // Enable alternate function
-    GPIO_PORTE_PCTL_R &= ~(0xFF << (TX*4));     // Clear PCTL bits
+    GPIO_PORTE_PCTL_R &= ~(0xF << (TX*4));     // Clear PCTL bits
     GPIO_PORTE_PCTL_R |= (1 << (TX*4));         // Set PCTL for UART
-    GPIO_PORTE_PCTL_R &= ~(0xFF << (RX*4));     // Clear PCTL bits
+    GPIO_PORTE_PCTL_R &= ~(0xF << (RX*4));     // Clear PCTL bits
     GPIO_PORTE_PCTL_R |= (1 << (RX*4));         // Set PCTL for UART
     
     // Configure UART5
@@ -65,23 +178,146 @@ void Init_BT(void) {
     UART5_CTL_R |= UART_CTL_UARTEN;     // Enable UART
 }
 
-int main(void) {
-    // Initialize all subsystems
-    Init_IO();
-    Init_PWM();
-    Init_BT();
-    
-    // Initialize state machine
-    initStateMachine();
-    
-    while(1) {
-        // Process bluetooth commands
-        processBluetoothCommand();
-        delay(1000);  // Small delay to prevent system overload
+char Bluetooth_Read(void)  
+{
+    char data;
+    while((UART5->FR & (1<<4)) != 0); /* wait until Rx buffer is not full */
+    data = UART5->DR;  	/* before giving it another byte */
+    return (unsigned char) data; 
+}
+
+void Bluetooth_Write(unsigned char data)  
+{
+    while((UART5->FR & (1<<5)) != 0); /* wait until Tx buffer not full */
+    UART5->DR = data;                  /* before giving it another byte */
+}
+
+void Bluetooth_Write_String(char *str)
+{
+    while(*str)
+    {
+        Bluetooth_Write(*(str++));
     }
 }
 
+void LED_Control(char direction)
+{
+    // Turn off all LEDs first
+    GPIOF->DATA &= ~0x0E;
+    
+    // Set LED based on direction
+    switch(direction) {
+        case 'W':  // Forward - Blue LED
+            GPIOF->DATA |= (1<<2);  // PF2 (Blue LED)
+            break;
+            
+        case 'S':  // Backward - Red LED
+            GPIOF->DATA |= (1<<1);  // PF1 (Red LED)
+            break;
+            
+        case 'A':  // Left - Green LED
+            GPIOF->DATA |= (1<<3);  // PF3 (Green LED)
+            break;
+            
+        case 'D':  // Right - All LEDs
+            GPIOF->DATA |= 0x0E;    // All LEDs
+            break;
+    }
+}
+
+void moveForward(long direction, unsigned long speed, unsigned int duration) {
+		// Direction range [-1000,+1000] -1000 corresponds to full left, +1000 full right, speed range [0,16k] effectively duty cycle if straight, duration in ms
+	
+    // Set all motors forward using H bridge while retaining enable settings
+		GPIO_PORTB_DATA_R &= (MASK(IN_24R)|MASK(IN_24L)|MASK(EN_R)|MASK(EN_L));
+		GPIO_PORTB_DATA_R |= (MASK(IN_24R)|MASK(IN_24L));
+	
+		if (direction >= 0){
+			// Right turn
+			R_CMP = CMP_Max-speed;
+			L_CMP = (unsigned short)( CMP_Max - (CMP_Max - pow(direction*CMP_Max/1000,2)/CMP_Max)*speed/16000);
+		}
+		else{
+			// Left turn
+			R_CMP = (unsigned short)( CMP_Max - (CMP_Max - pow(direction*CMP_Max/1000,2)/CMP_Max)*speed/16000);
+			L_CMP = CMP_Max-speed;
+		}
+		PWM0_ENABLE_R |= (PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Enable PWM0 outputs
+		delayms(duration);
+		PWM0_ENABLE_R &= ~(PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Disable PWM0 outputs
+}
+
+void moveBackward(long direction, unsigned long speed, unsigned int duration) {
+	  // inputs same as forward
+    // Set all motors backward using H bridge while retaining enable settings
+		GPIO_PORTB_DATA_R &= (MASK(IN_13R)|MASK(IN_13L)|MASK(EN_R)|MASK(EN_L));
+		GPIO_PORTB_DATA_R |= (MASK(IN_13R)|MASK(IN_13L));
+	
+		if (direction >= 0){
+			// Right turn
+			R_CMP = CMP_Max-speed;
+			L_CMP = (unsigned short)( CMP_Max - (CMP_Max - pow(direction*CMP_Max/1000,2)/CMP_Max)*speed/16000);
+		}
+		else{
+			// Left turn
+			R_CMP = (unsigned short)( CMP_Max - (CMP_Max - pow(direction*CMP_Max/1000,2)/CMP_Max)*speed/16000);
+			L_CMP = CMP_Max-speed;
+		}
+		PWM0_ENABLE_R |= (PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Enable PWM0 outputs
+		delayms(duration);
+		PWM0_ENABLE_R &= ~(PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Disable PWM0 outputs
+}
+
+void tankTurnLeft(long angle, unsigned int speed) {
+    // Right side forward, left side backward
+		GPIO_PORTB_DATA_R &= (MASK(IN_24R)|MASK(IN_13L)|MASK(EN_R)|MASK(EN_L));
+		GPIO_PORTB_DATA_R |= (MASK(IN_24R)|MASK(IN_13L));
+	
+		// set to given speed
+		L_CMP = CMP_Max-speed;
+		R_CMP = CMP_Max-speed;
+	
+		// calculate approx time to turn that far using an empircal constant
+		unsigned long angleDelay = labs(angle)/( spinK * (float)speed) ; 
+	
+		PWM0_ENABLE_R |= (PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Enable PWM0 outputs
+		delayms(angleDelay);
+		PWM0_ENABLE_R &= ~(PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Disable PWM0 outputs
+}
+
+void tankTurnRight(long angle, unsigned int speed) {
+    // Left side forward, right side backward
+		GPIO_PORTB_DATA_R &= (MASK(IN_13R)|MASK(IN_24L)|MASK(EN_R)|MASK(EN_L));
+		GPIO_PORTB_DATA_R |= (MASK(IN_13R)|MASK(IN_24L));
+	
+		// set to given speed
+		L_CMP = CMP_Max-speed;
+		R_CMP = CMP_Max-speed;
+	
+		// calculate approx time to turn that far using an empircal constant
+		unsigned long angleDelay = labs(angle)/( spinK * (float)speed) ; 
+	
+		PWM0_ENABLE_R |= (PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Enable PWM0 outputs
+		delayms(angleDelay);
+		PWM0_ENABLE_R &= ~(PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Disable PWM0 outputs
+} 
+
+void stopMotors(void) {
+    // Stop all motors, 
+		PWM0_ENABLE_R &= ~(PWM_ENABLE_PWM0EN|PWM_ENABLE_PWM1EN);  // Disable PWM0 outputs
+}
+
+
+void delayms(uint32_t count){
+    volatile uint32_t i;
+    for(i = 0; i < count; i++) {
+			delay(1058); // empirical constant for 1ms delay
+		}
+}
 void delay(uint32_t count) {
     volatile uint32_t i;
     for(i = 0; i < count; i++) {}
 }
+
+
+
--- project_code/main.h
+++ project_code/main.h
@@ -3,29 +3,32 @@
 #define MAIN_H
 
 #include <stdint.h>
-#include "C:\Keil_v5\Lab 2\inc/tm4c123gh6pm.h" // need to switch this to the correct header file
-
+#include <stdlib.h>
+#include <math.h>
+#include "TM4C123.h"
+#include "tm4c123gh6pm.h" // need to switch this to the correct header file
 // Utility macros
 #define MASK(x) (1UL << (x))
 
 // Motor control pins - Right motor
 #define IN_13R (0)    // Port B
 #define IN_24R (1)    // Port B
-#define EN_R  (5)     // Port B - PWM
+#define EN_R  (6)     // Port B - PWM
 
 // Motor control pins - Left motor
 #define IN_13L (2)    // Port B
 #define IN_24L (3)    // Port B
 #define EN_L  (7)     // Port B - PWM
 
+// duty cycles for the left and right motors, duty cycle = (CMP_Max - R_CMP)/CMP_Max
+#define R_CMP (PWM0_0_CMPA_R)
+#define L_CMP (PWM0_0_CMPB_R)
+#define CMP_Max (16667 - 1)
+#define spinK (0.000045)
+
 // Bluetooth module pins
 #define TX    (4)     // Port E
 #define RX    (5)     // Port E
 
-// Function prototypes
-void Init_IO(void);
-void Init_PWM(void);
-void Init_BT(void);
-void delay(uint32_t count);
 
 #endif // MAIN_H
